bb417aad4d03226f6047cc38a216ac79290285b4
onegate-finance <onegate_finance@protonmail.com>
2022-11-22 07:54:40 +0800

    update

--- x/auction/client/cli/tx.go
+++ x/auction/client/cli/tx.go
@@ -24,6 +24,7 @@ func GetTxCmd() *cobra.Command {
 
 	cmds := []*cobra.Command{
 		GetCmdPlaceBid(),
+		GetCmdPlaceBid2(),
 	}
 
 	for _, cmd := range cmds {
@@ -65,6 +66,45 @@ func GetCmdPlaceBid() *cobra.Command {
 				return err
 			}
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+}
+
+// GetCmdPlaceBid cli command for placing bids on auctions
+func GetCmdPlaceBid2() *cobra.Command {
+	return &cobra.Command{
+		Use:     "bid2 [auction-id] [amount]",
+		Short:   "place a bid on an auction",
+		Long:    "Place a bid on any type of auction, updating the latest bid amount to [amount]. Collateral auctions must be bid up to their maxbid before entering reverse phase.",
+		Example: fmt.Sprintf("  $ %s tx %s bid2 34 1000usdx 1010usdx --from myKeyName", version.AppName, types.ModuleName),
+		Args:    cobra.MinimumNArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return fmt.Errorf("auction-id '%s' not a valid uint", args[0])
+			}
+
+			var msgs []sdk.Msg
+			for _, v := range args[1:] {
+				amt, err := sdk.ParseCoinNormalized(v)
+				if err != nil {
+					return err
+				}
+
+				msg := types.NewMsgPlaceBid(id, clientCtx.GetFromAddress().String(), amt)
+				err = msg.ValidateBasic()
+				if err != nil {
+					return err
+				}
+				msgs = append(msgs, &msg)
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
 		},
 	}
 }
